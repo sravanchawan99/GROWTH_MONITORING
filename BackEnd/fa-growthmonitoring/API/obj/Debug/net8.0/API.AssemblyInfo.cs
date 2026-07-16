a7f2b9bccc7e1f9350201c85b71308f96710b981
Sravan Chawan <sravanchawan1999@gmail.com>
2026-07-17 01:22:38 +0530

    Made changes

--- BackEnd/fa-growthmonitoring/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ BackEnd/fa-growthmonitoring/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e479a01b06289122f992defba24ca58e71b9cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d321a6cd4132fe0132398780b0269cae97acb9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
