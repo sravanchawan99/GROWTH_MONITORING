8eced159f3c7f17de005dd1d2aa8fb8d8bacd996
Sravan Chawan <sravanchawan1999@gmail.com>
2026-09-04 21:21:47 +0530

    Added few logs and sorrounded the zscore with try catch block

--- BackEnd/fa-growthmonitoring/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ BackEnd/fa-growthmonitoring/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d321a6cd4132fe0132398780b0269cae97acb9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edd9ef4f2d51adfcb18d54c2b7dc5c5947b9967a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
